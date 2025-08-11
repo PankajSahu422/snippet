1bf77e073800ef47179aff989573a1509bfe815b
PankajSahu02 <sahupankaj01423@gmail.com>
2025-08-11 16:14:42 +0530

    snippet

---
+++ src/app/snippet/new/page.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { prisma } from '@/lib/prisma'
+import { redirect } from 'next/dist/server/api-utils'
+import React from 'react'
+
+const CreateSnippetPage = () => {
+
+  async function CreateSnippet(fromData:FormData) {
+    "use server" // use server direct
+    const title = fromData.get("title") as string;
+    const code = fromData.get("code") as string;
+    
+    const snippet = await prisma.snippet.create({
+      data:{
+        title,
+        code
+      }
+    });
+    console.log("created snippet", snippet);
+    
+    //redirect("/");
+
+  }
+  return (
+    <form action={CreateSnippet}>
+      <div>
+        <Label>Title</Label>
+        <Input type='text' name='title' id='title' />
+      </div>
+      <div>
+        <Label>Code</Label>
+        <Textarea name='code' id='code' />
+      </div>
+      <Button type='submit' className='my-4'>New</Button>
+    </form>
+  )
+}
+
+export default CreateSnippetPage
